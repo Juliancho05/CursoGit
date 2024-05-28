2dddf5c5f641abea7cc5144d7e74aae357850233
juliancho05 <juliancho055@gmail.com>
2024-05-28 16:02:47 -0500

    Add constructor in TaskGit

--- src/main/java/com/defaultpackage/tasks/TaskGit.java
+++ src/main/java/com/defaultpackage/tasks/TaskGit.java
@@ -4,6 +4,12 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 
 public class TaskGit implements Task {
+    private final String prueba;
+
+    public TaskGit(String prueba) {
+        this.prueba = prueba;
+    }
+
     @Override
     public <T extends Actor> void performAs(T actor) {
 
