99738e9b4a3a25c004ac44c5233dc05080ca34dd
juliancho05 <juliancho055@gmail.com>
2024-05-28 16:00:24 -0500

    Add UiGit and changes in InteractionGit

---
+++ src/main/java/com/defaultpackage/UI/UiGit.java
@@ -0,0 +1,12 @@
+package com.defaultpackage.UI;
+
+import org.checkerframework.checker.guieffect.qual.UI;
+
+import java.lang.annotation.Annotation;
+
+public class UiGit implements UI {
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
+    }
+}
--- src/main/java/com/defaultpackage/interactions/InteractionGit.java
+++ src/main/java/com/defaultpackage/interactions/InteractionGit.java
@@ -4,6 +4,12 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
 
 public class InteractionGit implements Interaction {
+    private final String name;
+
+    public InteractionGit(String name) {
+        this.name = name;
+    }
+
     @Override
     public <T extends Actor> void performAs(T actor) {
 
