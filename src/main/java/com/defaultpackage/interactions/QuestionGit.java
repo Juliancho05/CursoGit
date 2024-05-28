a15e283923f809e71d0993ec87c05de39b9b05b3
juliancho05 <juliancho055@gmail.com>
2024-05-28 12:07:31 -0500

    add QuestionGit

---
+++ src/main/java/com/defaultpackage/interactions/QuestionGit.java
@@ -0,0 +1,13 @@
+package com.defaultpackage.interactions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+public class QuestionGit implements Question {
+    private static String name;
+    @Override
+    public Object answeredBy(Actor actor) {
+        actor.attemptsTo();
+        return null ;
+    }
+}
