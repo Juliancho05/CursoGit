3a3390a32f6110167ba4104b1581ba2e2b1769f4
juliancho05 <juliancho055@gmail.com>
2024-05-29 09:44:35 -0500

    Add in Utils Methods branch Questions

---
+++ src/main/java/com/defaultpackage/utils/BD.java
@@ -0,0 +1,15 @@
+package com.defaultpackage.utils;
+
+public class BD {
+    public static void nameMethod(){
+
+    }
+
+    public void getInfo(){
+
+    }
+    public void setInfo(){
+        
+    }
+
+}
